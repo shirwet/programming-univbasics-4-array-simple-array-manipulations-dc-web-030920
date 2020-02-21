1170efdb64c3fdf880ebd4582b3e6805f0e4bbbc
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-21 23:55:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,7 +28,7 @@ def shift_with_args (array)
 array.shift(2)
 end
 
-def using_concat (my_favorite_things, array2)
+def using_concat (my_favorite_things, more_favs)
 my_favorite_things= ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["mario kart", "flatiron school"]
 my_favorite_things.concat (more_favs)
