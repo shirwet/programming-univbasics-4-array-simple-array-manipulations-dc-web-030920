ae6f62e608ee4c730bd374073830382695aba42a
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-21 23:51:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,6 +31,6 @@ end
 def using_concat (array1, array2)
 my_favorite_things= ["raindrops on roses", "whiskers on kittens"]
 my_favorite_things.concat (["sports cars", "flatiron school"])
-my_favorite_things.length
+
 end
 
