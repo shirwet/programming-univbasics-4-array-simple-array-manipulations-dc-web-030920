46996696a98969a0be17405b00c5de82613361de
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-21 23:45:29 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_push (array, string)
+adding_color = "violet"
+ array.push(adding_color) 
+end
+
+def using_unshift (array, string)
+new_neighborhood = "Staten Island"
+ array.unshift(new_neighborhood)
+end
+
+def using_pop (array)
+
+array.pop
+end
+
+def pop_with_args (array)
+
+array.pop(2)
+end
+
+def using_shift (array)
+
+array.shift
+end
+
+def shift_with_args (array)
+
+array.shift(2)
+end
+
+def using_concat (array1, array2)
+my_favorite_things= ["raindrops on roses", "whiskers on kittens"]
+my_favorite_things.concat (["sports cars", "flatiron school"])
+end
+
